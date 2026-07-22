98006fbbd7a62707a87f72e02f791bf53138e5da
Thameem94 <ashiqthameem94@gmail.com>
2026-07-22 10:24:05 +0530

    Add files via upload
    Day01HomeAssignment

---
+++ day01Homeassignment.js
@@ -0,0 +1,6 @@
+let firstname = 'Thameem'
+let CompanyName = 'xyz'
+let MoblieNumber = 123456789
+let isautomation = true
+let hasplaywright =
+console.log(typeof firstname, typeof CompanyName, typeof MoblieNumber, typeof isautomation, typeof hasplywrightn);
